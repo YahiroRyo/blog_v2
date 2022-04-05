2bf2f875d5afc832733c7654c4f07c8dcc7625d9
YahiroRyo <honakafn@gmail.com>
2022-04-06 04:24:12 +0900

    テスト用のデータ作れるようにした

---
+++ api/database/factories/WorkFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Work>
+ */
+class WorkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->title(),
+            "description" => $this->faker->realText(),
+            "contents" => $this->faker->realText(),
+        ];
+    }
+}
--- api/database/seeders/WorkSeeder.php
+++ api/database/seeders/WorkSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Work;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class WorkSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            BlogSeeder::class,
-            WorkSeeder::class,
-        ]);
+        Work::factory(50)->create();
     }
 }
