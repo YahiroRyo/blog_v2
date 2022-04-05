e71f0fea1691308bfb2a374aab93033225f0062e
YahiroRyo <honakafn@gmail.com>
2022-04-06 01:44:42 +0900

    blogやworkをcreateする画面を作った

--- client/components/commons/Input.tsx
+++ client/components/commons/Input.tsx
@@ -16,6 +16,9 @@ const Input = ({id, className, type = "text", children, onChange}: Props) => {
         case "button":
             input = <button id={id} className={Str.joinClassName(styles.button, className ? className : '')}>{children}</button>
             break;
+        case "textarea":
+            input = <textarea id={id} className={Str.joinClassName(styles.textarea, className ? className : '')} />
+            break;
         default:
             input = <input
                 id={id}
---
+++ client/pages/admin/blogs/create.tsx
@@ -0,0 +1,11 @@
+import Create from "../../../components/commons/Craete";
+
+const BlogCreate = () => {
+    return (
+        <Create
+            isBlog={true}
+        />
+    )
+}
+
+export default BlogCreate;
