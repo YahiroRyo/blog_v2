25ca99983f230dccedcff633b2e4da962c98b4ed
YahiroRyo <honakafn@gmail.com>
2022-04-06 18:38:57 +0900

    ページ見れるようにした

---
+++ client/components/commons/DetailArticle.tsx
@@ -0,0 +1,37 @@
+import ReactMarkdown from "react-markdown";
+import CommonMeta from "./CommonMeta";
+import styles from "../../styles/components/commons/DetailArticle.module.scss";
+import Image from "next/image";
+import Str from "../../utils/str";
+
+type Props = {
+    title: string,
+    description: string,
+    contents: string,
+    createdAt: string,
+};
+
+const DetailArticle = ({title, description, contents, createdAt}: Props) => {
+    const getCreatedAt = () => {
+        const date = new Date(createdAt);
+        return `${date.getFullYear()}年 ${date.getMonth() + 1}月${date.getDate()}日`
+    }
+    
+    return (
+        <div className={Str.joinClassName(styles.markdown, "inner")}>
+            <CommonMeta title={title} description={description} />
+            <h1 className={styles.markdown__title}>{title}</h1>
+            <span className={styles.markdown__date}>
+                <div className={styles.markdown__date__icon}>
+                    <Image src="/icons/clock-black.svg" layout="fill" alt="clock" loading="lazy"/>
+                </div>
+                <span className={styles.markdown__date__text}>{getCreatedAt()}</span>
+            </span>
+            <ReactMarkdown>
+                    {contents}
+            </ReactMarkdown>
+        </div>
+    )
+}
+
+export default DetailArticle;
